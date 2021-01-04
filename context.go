d43216fa53a36dfc1ea706b474c763bbcba4bf0f
Eric Fritz <eric@eric-fritz.com>
2021-01-04 16:42:33 -0600

    Add context helpers (#1)

---
+++ context.go
@@ -0,0 +1,76 @@
+package glock
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+type glockAwareContext struct {
+	context.Context
+	mu   sync.Mutex
+	err  error
+	done chan struct{}
+}
+
+// ContextWithDeadline mimmics context.WithDeadline, but uses the given clock instance
+// instead of the using standard time.After function directly.
+func ContextWithDeadline(ctx context.Context, clock Clock, deadline time.Time) (context.Context, context.CancelFunc) {
+	return ContextWithTimeout(ctx, clock, deadline.Sub(clock.Now()))
+}
+
+// ContextWithTimeout mimmics context.WithTimeout, but uses the given clock instance
+// instead of the using standard time.After function directly.
+func ContextWithTimeout(ctx context.Context, clock Clock, timeout time.Duration) (context.Context, context.CancelFunc) {
+	done := make(chan struct{})
+	canceled := make(chan struct{})
+
+	ctx, cancel := context.WithCancel(ctx)
+	child := &glockAwareContext{Context: ctx, done: done}
+
+	go func() {
+		defer cancel()
+		defer close(done)
+
+		child.setErr(watchContext(ctx, canceled, clock.After(timeout)))
+	}()
+
+	return child, closeOnce(canceled)
+}
+
+func (ctx *glockAwareContext) Done() <-chan struct{} {
+	return ctx.done
+}
+
+func (ctx *glockAwareContext) Err() error {
+	ctx.mu.Lock()
+	defer ctx.mu.Unlock()
+	return ctx.err
+}
+
+func (ctx *glockAwareContext) setErr(err error) {
+	ctx.mu.Lock()
+	defer ctx.mu.Unlock()
+	ctx.err = err
+}
+
+func watchContext(ctx context.Context, canceled <-chan struct{}, afterCh <-chan time.Time) error {
+	select {
+	case <-canceled:
+		return context.Canceled
+	case <-afterCh:
+		return context.DeadlineExceeded
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
+func closeOnce(ch chan<- struct{}) context.CancelFunc {
+	var once sync.Once
+
+	return func() {
+		once.Do(func() {
+			close(ch)
+		})
+	}
+}
---
+++ context_test.go
@@ -0,0 +1,100 @@
+package glock
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContext(t *testing.T) {
+	testContextWithTimeout(t, func(state *contextTestState) {})
+}
+
+func TestContextCancelParent(t *testing.T) {
+	testContextWithTimeout(t, func(state *contextTestState) {
+		state.err1 = context.Canceled
+		state.err2 = context.Canceled
+		state.err3 = context.Canceled
+		state.cancel1()
+	})
+}
+
+func TestContextCancel(t *testing.T) {
+	testContextWithTimeout(t, func(state *contextTestState) {
+		state.err2 = context.Canceled
+		state.err3 = context.Canceled
+		state.cancel2()
+	})
+}
+
+func TestContextCancelChild(t *testing.T) {
+	testContextWithTimeout(t, func(state *contextTestState) {
+		state.err3 = context.Canceled
+		state.cancel3()
+	})
+}
+
+func TestContextDeadline(t *testing.T) {
+	testContextWithTimeout(t, func(state *contextTestState) {
+		state.err2 = context.DeadlineExceeded
+		state.err3 = context.DeadlineExceeded
+		state.clock.BlockingAdvance(time.Second)
+	})
+}
+
+type contextTestState struct {
+	clock   *MockClock
+	ctx1    context.Context
+	ctx2    context.Context
+	ctx3    context.Context
+	cancel1 context.CancelFunc
+	cancel2 context.CancelFunc
+	cancel3 context.CancelFunc
+	err1    error
+	err2    error
+	err3    error
+}
+
+func testContextWithTimeout(t *testing.T, f func(state *contextTestState)) {
+	clock := NewMockClock()
+
+	ctx := context.Background()
+	ctx1, cancel1 := context.WithCancel(ctx)
+	ctx2, cancel2 := ContextWithTimeout(ctx1, clock, time.Second)
+	ctx3, cancel3 := context.WithCancel(ctx2)
+	defer cancel1()
+	defer cancel2()
+	defer cancel3()
+
+	state := &contextTestState{
+		clock:   clock,
+		ctx1:    ctx1,
+		ctx2:    ctx2,
+		ctx3:    ctx3,
+		cancel1: cancel1,
+		cancel2: cancel2,
+		cancel3: cancel3,
+	}
+
+	assertDoneAndErr(t, ctx1, nil)
+	assertDoneAndErr(t, ctx2, nil)
+	assertDoneAndErr(t, ctx3, nil)
+
+	f(state)
+
+	assertDoneAndErr(t, ctx1, state.err1)
+	assertDoneAndErr(t, ctx2, state.err2)
+	assertDoneAndErr(t, ctx3, state.err3)
+}
+
+func assertDoneAndErr(t *testing.T, ctx context.Context, err error) {
+	if err != nil {
+		eventually(t, structChanReceives(ctx.Done()))
+		assert.Equal(t, ctx.Err(), err)
+		return
+	}
+
+	consistently(t, structChanDoesNotReceive(ctx.Done()))
+}
