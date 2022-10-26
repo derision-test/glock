5aecf9d6ccc257b68a6431d412395e7815d80b16
Kristin Davidson <kdavidson@atlassian.com>
2022-10-26 14:25:57 -0500

    Add documentation for Timer type

--- mock_timer.go
+++ mock_timer.go
@@ -8,6 +8,9 @@ func sendTime(t *MockTimer) {
 	t.ch <- t.now
 }
 
+// MockTimer is an implementation of Timer that can be moved forward in time
+// in increments for testing code that relies on timeouts or other time-sensitive
+// constructs.
 type MockTimer struct {
 	*advanceable
 	deadline time.Time
@@ -19,20 +22,26 @@ type MockTimer struct {
 var _ Timer = &MockTimer{}
 var _ Advanceable = &MockTimer{}
 
+// NewTimer creates a new Timer tied to the internal MockClock time that functions
+// similar to time.NewTimer().
 func (c *MockClock) NewTimer(duration time.Duration) Timer {
 	return newMockTimerAt(c.advanceable, duration, sendTime)
 }
 
+// AfterFunc creates a new Timer tied to the internal MockClock time that functions
+// similar to time.AfterFunc().
 func (c *MockClock) AfterFunc(duration time.Duration, f func()) Timer {
 	return newMockTimerAt(c.advanceable, duration, func(mt *MockTimer) {
 		go f()
 	})
 }
 
+// NewMockTimer creates a new MockTimer with the internal time set to time.Now().
 func NewMockTimer(duration time.Duration) *MockTimer {
 	return NewMockTimerAt(time.Now(), duration)
 }
 
+// NewMockTimerAt creates a new MockTimer with the internal time set to the given time.
 func NewMockTimerAt(now time.Time, duration time.Duration) *MockTimer {
 	return newMockTimerAt(newAdvanceableAt(now), duration, sendTime)
 }
@@ -59,10 +68,15 @@ func newMockTimerAt(
 	return t
 }
 
+// Chan returns a channel which will receive the Timer's internal time at the
+// point where the Timer is triggered.
 func (t *MockTimer) Chan() <-chan time.Time {
 	return t.ch
 }
 
+// Reset will reset the deadline of the Timer to trigger after the new duration
+// based on the Timer's internal current time. If the Timer was running when Reset
+// was called it will return true.
 func (t *MockTimer) Reset(duration time.Duration) bool {
 	t.cond.L.Lock()
 	defer t.cond.L.Unlock()
@@ -82,6 +96,7 @@ func (t *MockTimer) Reset(duration time.Duration) bool {
 	return wasRunning
 }
 
+// Stop will stop the Timer from running.
 func (t *MockTimer) Stop() bool {
 	t.cond.L.Lock()
 	defer t.cond.L.Unlock()
@@ -96,6 +111,10 @@ func (t *MockTimer) Stop() bool {
 	return true
 }
 
+// BlockingAdvance will bump the timer's internal time by the given duration. If
+// the new internal time passes the timer's trigger threshold, a signal will be sent.
+// This method will not return until the signal is read by a consumer of the Timer's
+// channel.
 func (t *MockTimer) BlockingAdvance(duration time.Duration) {
 	t.m.Lock()
 	defer t.m.Unlock()
@@ -103,7 +122,6 @@ func (t *MockTimer) BlockingAdvance(duration time.Duration) {
 	t.now = t.now.Add(duration)
 
 	t.tryExecute()
-	// TODO Test this
 }
 
 func (t *MockTimer) tryExecute() {
@@ -126,6 +144,7 @@ func (t *MockTimer) process() {
 
 }
 
+// signal conforms to the subscriber interface.
 func (t *MockTimer) signal(now time.Time) bool {
 	return !t.stopped
 }
--- timer.go
+++ timer.go
@@ -2,9 +2,18 @@ package glock
 
 import "time"
 
+// Timer is a wrapper around a time.Timer, which allows interface access to
+// the underlying Timer.
 type Timer interface {
+	// Chan returns the underlying timer channel.
 	Chan() <-chan time.Time
 
+	// Reset will reset the duration of the timer to the new duration. If
+	// the Timer was running when Reset was called it will return true. For
+	// more information about when Reset can be called, see the time.Timer
+	// documentation.
 	Reset(d time.Duration) bool
+	// Stop stops the timer. If the timer was running when Stop was
+	// called it will return true.
 	Stop() bool
 }
